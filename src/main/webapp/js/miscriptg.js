5e8a83515268d74e8c27c8db2857579f13c47fc5
jesus <jesfeoro@gmail.com>
2015-07-09 12:58:35 +0200

    añadido css, js, images, bootstrap

---
+++ src/main/webapp/js/miscriptg.js
@@ -0,0 +1,188 @@
+function reloadCaptcha(){
+    var d = new Date();
+    $("#captcha_image").attr("src", "EdwCaptcha?"+d.getTime());
+}
+
+$(document).ready(function(){
+	
+    var jumboHeight = $('.jumbotron').outerHeight();
+    function parallax(){
+        var scrolled = $(window).scrollTop();
+        $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+    }
+
+    $(window).scroll(function(e){
+        parallax();
+    }); 
+    console.log(" hola mensaje en consola");
+    console.info(" hola mensaje informativo");
+    console.warn(" hola mensaje de aviso");  
+    console.error(" hola mensaje de error");
+    console.debug(" hola mensaje de debug");
+	$(function() {
+		 
+	    $("#spotlight").autocomplete({
+			source: function(request, response) {
+            $.ajax({
+                    url : "ControladorAuto",
+                    type : "GET",
+                    data : {
+                            term : request.term
+                    },
+                    dataType : "json",
+                    success : function(data) {
+                            response(data);
+                    }
+            });},
+			appendTo: $("form:first")
+		});
+	 
+		$("#spotlight").data( "ui-autocomplete" )._renderMenu = function( ul, items ) {
+			var that = this;		
+			ul.attr("class", "nav nav-pills nav-stacked");
+			$.each( items, function( index, item ) {
+				that._renderItemData( ul, item );
+			});
+		};	    
+	 
+	});
+	
+	$('#olvidado').click(function(e) {
+        e.preventDefault();
+        $('div#form-olvidado').toggle('500');
+      });
+      $('#acceso').click(function(e) {
+        e.preventDefault();
+        $('div#form-olvidado').toggle('500');
+      });
+	
+	//Para poner en el mismmo servlet la comprobación de usuario y el email
+	// con el imput hidden de comprobar
+	$("#usuario").focus(function() {
+        var oID = $(this).attr("id");
+        $("#comprobar").val(oID);
+        });
+	$("#email").focus(function() {
+        var oID = $(this).attr("id");
+        $("#comprobar").val(oID);
+        });
+	
+	jQuery.fn.reset = function () {
+		  $(this).each (function() { this.reset(); });
+		}
+	
+    $.validator.addMethod("regex",function(value,element,regexp){
+        var re= new RegExp(regexp);
+        return this.optional(element) || re.test(value);
+    },"Solo caracteres alfanumericos");
+    
+    $("#NuevoUser").validate({
+        rules:{
+                usuario:{
+                    required:true,
+                    regex:"^[a-zA-Z0-9_]+$",
+                    remote:"UserCorrecto"
+                },
+                email:{
+                    email:true,
+                    required:true
+                },
+                answer:{
+                    required:true,
+                    remote:"EnvioCaptcha"
+                },
+                pass:{
+                	required: true,
+                	minlength: 3,
+                	maxlength: 8
+                },
+                pass2:{
+                	required: true,
+                    equalTo:"#pass",
+                    minlength: 3,
+                    maxlength: 8
+                }
+        },
+        messages:{
+            usuario:{
+                required:"Campo obligatorio",
+                	remote:"Ese nombre esta en uso."
+            },
+            pass:{
+                required:"Campo obligatorio",
+                minlength:"minimo 3 caracteres",
+                maxlength:"maximo 8 caracteres"
+            },
+            pass2:{
+            	required:"Campo obligatorio",
+                equalTo:"La contraseña no es igual",
+                minlength:"minimo 3 caracteres",
+                maxlength:"maximo 8 caracteres"
+            },
+            answer:{
+                required:"Campo es obligatorio",
+                remote:"Captcha incorrecto. Click en refrescar para uno nuevo"
+            },
+            email:{
+                email:"El email no es valido",
+                required:"Campo obligatorio"
+            }
+        },
+        highlight: function(element) {
+           // var id_attr = "#" + $( element ).attr("id") + "1";
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            $(element).parent().find('.form-control-feedback').removeClass('glyphicon-ok').addClass('glyphicon-remove');        
+        },
+        unhighlight: function(element) {
+           // var id_attr = "#" + $( element ).attr("id") + "1";
+            $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+            $(element).parent().find('.form-control-feedback').removeClass('glyphicon-remove').addClass('glyphicon-ok');        
+        },
+        errorElement: 'span',
+            errorClass: 'help-block',
+            errorPlacement: function(error, element) {
+                if(element.length) {
+                    error.insertAfter(element);
+                } else {
+                error.insertAfter(element);
+                }
+            } ,
+        success: function(element) {
+				$(element).closest('.form-group').removeClass('has-error');
+			},
+        submitHandler:function(form){
+        	//Crearemos  el registro en la base de datos
+        	$.ajax({
+				type: $(form).attr("method"),
+				url: $(form).attr("action"),
+				data: $(form).serialize(),
+				dataType : "json",
+				success: function(result){
+					//Aqui se evalua result, si tenemos fallos en la inserccion en la base de datos (true o false)
+		            $('#RegistroModal').modal('hide');          
+		            // borraremos las classes de colores y los glyphicons que teniamos en el formulario de ok
+		            $('.form-group').removeClass('has-error has-feedback');
+		            $('.form-group').removeClass('has-success has-feedback');
+		            $('.form-control-feedback').removeClass('glyphicon-ok');
+					// borramos los datos del formulario
+		            $("#NuevoUser").reset();	
+					// ahora creamos un alert para mostrar el resultado del registro insertado con exito
+		            $('#barra').before('<div class="alert alert-success" id="success-alert">'+
+		            	    '<a href="#" class="close" data-dismiss="alert">&times;</a>'+
+		            	   ' <strong>Bienvenido!</strong>Ya Puedes acceder a descubrir mas cosas de este sitio.'+
+		            	'</div>	');
+					// por si no quiere cerrar la ventana se cerrara automaticamente
+		            $("#success-alert").fadeTo(7000, 500).slideUp(500, function(){
+		                $("#success-alert").alert('close');
+		            });
+					
+				},
+
+ 				       	
+        	})
+            
+            
+        }
+        	
+    })
+})
---
+++ src/main/webapp/js/scripts.js
@@ -0,0 +1,278 @@
+﻿window.carousel_header = {
+    links: [],
+    _position: 0,
+    prev: function () {
+        if (this._position == 0) {
+            this._position = this.links.length;
+        }
+        this._position--;
+        this.go();
+    },
+    next: function () {
+        if (this._position == this.links.length - 1) {
+            this._position = -1;
+        }
+        this._position++;
+        this.go();
+    },
+    _initImages: function (imgHolder) {
+        var imgHolder = document.getElementById("carousel_active_href");
+        var controls = document.getElementById("carousel_controls");
+       // controls.style.width = 19 * this.links.length + "px";
+        for (var i = 0; i < this.links.length; i++) {
+            var img = document.createElement("img");
+            img.src = this.links[i].src;
+            img.alt = this.links[i].alt || "";
+            img.title = this.links[i].alt || "";
+            img.style.display = "none";
+            imgHolder.appendChild(img);
+
+            var button = document.createElement("div");
+            button.className = "control";
+            controls.appendChild(button);
+        }
+
+
+    },
+    autoswitch: function () {
+        if (this._timeout)
+            clearTimeout(this._timeout);
+        var self = this;
+        this._timeout = setTimeout(function () {
+            self.next();
+        }, 15 * 1000);
+    },
+    init: function (config) {
+        if (config) {
+            this.links = config;
+        }
+        this.go();
+    },
+    go: function (index) {
+        if (index === undefined) {
+            index = this._position;
+        } else {
+            this._position = index;
+        }
+
+        var link = this.links[index];
+        var imgHolder = document.getElementById("carousel_active_href");
+        imgHolder.href = link.href;
+
+        var items = imgHolder.getElementsByTagName("img");
+        if (items.length < this.links.length) {
+            this._initImages();
+        }
+
+        for (var i = 0; i < items.length; i++) {
+            if (i != index) {
+                items[i].style.display = "none";
+            } else {
+                items[i].style.display = "";
+            }
+        }
+        var self = this;
+        setTimeout(function () {
+            self._updatePointers(index)
+        }, 1);
+        this.autoswitch();
+
+    },
+    _updatePointers: function (index) {
+        if (index === undefined) {
+            index = this._position;
+        }
+        var cts = document.getElementById("carousel_controls").getElementsByTagName("div");
+        for (var i = 0; i < cts.length; i++) {
+            if (i != index) {
+                cts[i].className = cts[i].className.replace(/ active/g, "");
+
+                (function (temp) {
+                    cts[temp].onclick = function () { carousel_header.go(temp) };
+                })(i)
+
+            } else {
+                cts[i].className += " active";
+                cts[i].onclick = null;
+            }
+        }
+
+    }
+
+};
+
+window.popupTypes = { bug: 0, request: 1, feedback:2 };
+function getFeedback(type) {
+
+    var options = [
+        { value:"bug_report", label:"Report a bug" },
+        { value:"feature_request", label:"Request a feature" },
+        { value:"feedback", label:"Leave a feedback"}];
+    var opts = "";
+    for (var i = 0; i < options.length; i++) {
+        opts += ["<option value=\"",  options[i].value ,"\"", (i===type ? " selected " : ""), ">", options[i].label, "</option>"].join('');
+    }
+
+
+    var box = window.temp_box = dhtmlx.modalbox({
+        title: "Contact Us",
+        text: "<div id='form_in_box'>" +
+                "<div class=\"form-block\"><div class=\"box_label\"> Email </div><div class=\"box_input\"><input title=\"Enter your name or email address\" name=\"author\" type='text'></div></div>" +
+                "<div class=\"form-block\"><div class=\"box_label\"> Subject </div><div class=\"box_input\"><select name=\"type\" type='text'>" + opts + "</select></div></div>" +
+                "<div class=\"form-block\"><div class=\"box_label\"> Summary </div><div class=\"box_input\"><textarea rows=\"4\"></textarea></div></div>" +
+                "<div class=\"form-block \"><span class=\"dhtmlx_button\"><input type=\"button\" onclick=\"hideBox(this, true)\" value=\"Send\" style=\"background-color: #6fbb01 !important;color: white;border-color: #6fbb01 !important;\"/></span><span class=\"dhtmlx_button\"><input type=\"button\"  onclick=\"hideBox(this, false)\" value=\"Cancel\"/></span></div>",
+        width: "450px",
+        ok: "Send",
+        type:"contact_us_form",
+        cancel: "Cancel"
+    });
+    if (document.attachEvent)
+        document.attachEvent("onkeydown", contact_mod_key);
+    else
+        document.addEventListener("keydown", contact_mod_key, true);
+
+    //setTimeout(function () {
+    //    box.getElementsByTagName("input")[0].focus();
+    //}, 1);
+}
+function contact_mod_key(e) {
+
+        e = e || event;
+        var code = e.which || event.keyCode;
+  
+        if (code == 27) {
+            hideBox(window.temp_box);
+        }
+    }
+    
+function hideBox(box, result) {
+    if (result) {       
+
+        while (box.parentNode && box.id != "form_in_box") {
+            box = box.parentNode;
+        }
+        if (box) {
+            var authBox = box.getElementsByTagName("input")[0];
+            var contentBox = box.getElementsByTagName("textarea")[0];
+
+            var author = authBox.value;
+            var type = box.getElementsByTagName("select")[0].value;
+            var content = contentBox.value;
+
+            var error_color = "#FBDBDB";
+
+            if (!(author && content)) {
+                if (!author) {
+                    authBox.style.backgroundColor = error_color;
+                }
+                if (!content) {
+                    contentBox.style.backgroundColor = error_color;
+                }
+                return;
+            }
+
+            contentBox.style.backgroundColor = "";
+            authBox.style.backgroundColor = "";
+
+            var params = ["author=" + author, "type=" + type, "message=" + content, "page=" + window.location.href].join("&");
+            sendRequest("/common/contact.ashx", params, "POST");
+        }
+        dhtmlx.message("Thank you for your feedback!");
+    }
+    
+    if (document.attachEvent)
+        document.detachEvent("onkeydown", contact_mod_key);
+    else
+        document.removeEventListener("keydown", contact_mod_key, true);
+    window.temp_box = null;
+    dhtmlx.modalbox.hide(box);
+    
+
+}
+
+
+function sendRequest(url, params, type) {
+    var req;
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+    }
+    else {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    req.onreadystatechange = function () { }
+    req.open(type, url, true);
+    if (type == "POST") {
+        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    }
+    req.send(params || null);
+}
+
+
+// popup search
+
+function showSearch() {
+    document.getElementById("search-popup").style.display = "block";
+}
+
+function hideSearch() {
+    document.getElementById("search-popup").style.display = "none";
+}
+
+// to top move
+
+(function () {
+
+    function $(id) {
+        return document.getElementById(id);
+    }
+
+    window.goTop = function(acceleration, time) {
+        acceleration = acceleration || 0.1;
+        time = time || 12;
+
+        var dx = 0;
+        var dy = 0;
+        var bx = 0;
+        var by = 0;
+        var wx = 0;
+        var wy = 0;
+
+        if (document.documentElement) {
+            dx = document.documentElement.scrollLeft || 0;
+            dy = document.documentElement.scrollTop || 0;
+        }
+        if (document.body) {
+            bx = document.body.scrollLeft || 0;
+            by = document.body.scrollTop || 0;
+        }
+        var wx = window.scrollX || 0;
+        var wy = window.scrollY || 0;
+
+        var x = Math.max(wx, Math.max(bx, dx));
+        var y = Math.max(wy, Math.max(by, dy));
+
+        var speed = 1 + acceleration;
+        window.scrollTo(Math.floor(x / speed), Math.floor(y / speed));
+        if (x > 0 || y > 0) {
+            window.setTimeout(function(){
+                goTop(acceleration, time);
+            }, time);
+        }
+        return false;
+    }
+
+
+})();
+
+
+/**
+* String.trim polyfill from here
+* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+**/
+if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+        // Make sure we trim BOM and NBSP
+        rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        return this.replace(rtrim, "");
+    }
+}
